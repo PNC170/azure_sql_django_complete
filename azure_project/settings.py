e6ea56cf4648f61a77757818551e2403388412a8
PNC170 <paowattanun@gmail.com>
2026-02-26 18:48:06 +0700

    Update settings.py

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -105,11 +105,11 @@ DATABASES = {
 import urllib.parse
 
 # MongoDB Configuration
-mongo_username = urllib.parse.quote_plus('bearlab')
+mongo_username = urllib.parse.quote_plus('aumadmin')
 mongo_password = urllib.parse.quote_plus('P@ss1234')
 # MONGO_URI = 'mongodb://localhost:27017/'
-MONGO_URI = f'mongodb+srv://{mongo_username}:{mongo_password}@bearlab-mongodb.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
-MONGO_DB_NAME = 'django_store_reviews'
+MONGO_URI = f'mongodb+srv://{mongo_username}:{mongo_password}@aummongodb.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
+MONGO_DB_NAME = 'aummongodb'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
